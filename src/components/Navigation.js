1cb4e9c50b9d4da3738db630531c729033ce8a6c
royFloresNyc <royfloresnyc@gmail.com>
2021-02-12 23:39:59 -0500

    Fix Social Media Icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Container, Navbar, Nav, Image } from 'react-bootstrap'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import GitHubIcon from '@material-ui/icons/GitHub'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { FaGithub, FaMedium, FaLinkedin } from 'react-icons/fa'
 
-import Medium from '../assets/medium-icon_7.png'
 
 const Navigation = ({ modalHandler }) => {
     return (
@@ -13,13 +11,13 @@ const Navigation = ({ modalHandler }) => {
                     <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                     <div className='justify-content-end order-sm-last'>
                             <a className='btn btn-social-icon' href='https://github.com/royFloresNyc' target='_blank' rel='noreferrer'>
-                                <GitHubIcon style={{color: 'white'}}/>
+                                <FaGithub style={{color: 'white', fontSize: 25}}/>
                             </a>
                             <a className='btn btn-social-icon' href='https://royfloresnyc.medium.com/' target='_blank' rel='noreferrer'>
-                                <Image src={Medium} roundedCircle fluid style={{width: 32, height: 32}}/>
+                                <FaMedium style={{color: 'white', fontSize: 25}}/>
                             </a>
                             <a className='btn btn-social-icon' href='https://www.linkedin.com/in/roy-flores-5275601ab/' target='_blank' rel='noreferrer'>
-                                <LinkedInIcon style={{color: 'white', fontSize: 32}}/>
+                                <FaLinkedin style={{color: 'white', fontSize: 25}}/>
                             </a>
                     </div>
                     <Navbar.Collapse id='responsive-navbar-nav'>
